b3c55111fbeda4582d08aa23a92bf123961e8266
elle226 <elle.226.725@gmail.com>
2018-01-12 21:52:11 +0900

    1/12

--- Java/TestProject01/src/HelloWorld.java
+++ Java/TestProject01/src/HelloWorld.java
@@ -2,15 +2,13 @@
 public class HelloWorld {
 
 	/**
-	 * @param args 
+	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		int suugaku = 45;
-		int eigo = 82;
-		if (suugaku > 50 || eigo > 50){
-			System.out.println("この人は合格です");
-		}
-	}
 
+		double a2 = (double) 10 / 3;
+		System.out.println(a2);
+
+
+	}
 }
