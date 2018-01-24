056ac3a00bb3a7e8e8e3cf68c34f55007efc076c
elle226 <elle.226.725@gmail.com>
2018-01-24 21:38:29 +0900

    1/24

---
+++ Java/Iphone/src/Iphone.java
@@ -0,0 +1,18 @@
+
+public class Iphone {
+
+	public static void main(String[] args) {
+		SmartPhone iPhone = new SmartPhone () ;
+		iPhone.play ();
+		iPhone.stop ();
+		iPhone.next ();
+		iPhone.back ();
+		iPhone.call ();
+		iPhone.mail ();
+		iPhone.photo ();
+		iPhone.internet ();
+
+
+	}
+
+}
