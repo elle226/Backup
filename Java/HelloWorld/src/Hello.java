c471096248b55edad2934ac538601c58a5da4e46
elle226 <elle.226.725@gmail.com>
2018-01-24 12:32:08 +0900

    1/24

---
+++ Java/HelloWorld/src/Hello.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Hello {
+
+	public static void main(String[] args) throws IOException{
+		String s = "123";
+		int i = Integer.parseInt(s);
+		System.out.println(i);
+	}
+
+}
